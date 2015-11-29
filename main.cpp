35c7ecf9cba79526da2bd8de9f4e03de0dc70ba1
BJakab <jakab2008@gmail.com>
2015-11-29 05:43:29 +0100

    tests should be fine now

--- main.cpp
+++ main.cpp
@@ -14,14 +14,14 @@ void test_value(std::string stringified, Function func, int id, double at, doubl
 	double val = ctd::value(func, id, at);
 	char strVal[512];
 	char strExpected[512];
-	
+
 	std::sprintf(strVal, "%.10f", val);
 	std::sprintf(strExpected, "%.10f", expected);
 	int maxLen = std::min(strlen(strVal), strlen(strExpected));
-	
+
 	int errPos = -1;
 	for (int i = 1; i < maxLen && (errPos = i) && std::strncmp(strVal, strExpected, i) == 0; i++);
-	
+
 	if (errPos == -1){
 		output << "[OK]\n";
 	}
@@ -37,36 +37,36 @@ void test_value(std::string stringified, Function func, int id, double at, doubl
 #define TEST(FUNC,ID,AT,EX,OUT) test_value((#FUNC), (FUNC), (ID), (AT), (EX), (OUT))
 
 int main(int argc, char** argv) {
-    
+
 	// using the namespace for shorter code
 	using namespace ctd;
 
-	TEST(    x,  'x', 5,   6, std::cout);
-	TEST(    x,  'y', 5, 6.1, std::cout);
-	TEST(1 + x,  'x', 5, 6.1, std::cout);
-	TEST(x + 1,  'x', 5, 6.1, std::cout);
-	TEST(2 * x,  'x', 5, 6.1, std::cout);
-	TEST(x * 2,  'x', 5, 6.1, std::cout);
-	TEST(x * x,  'x', 5, 6.1, std::cout);
-	TEST(1 / x,  'x', 5, 6.1, std::cout);
-	TEST(x / 1,  'x', 5, 6.1, std::cout);
-	TEST(x / x,  'x', 5, 6.1, std::cout);
-	TEST(x ^ 2,  'x', 5, 6.1, std::cout);
-	TEST(2 ^ x,  'x', 5, 6.1, std::cout);
-	TEST(x ^ x,  'x', 5, 6.1, std::cout);
-	TEST(sin(x), 'x', 5, 6.1, std::cout);
-	TEST(cos(x), 'x', 5, 6.1, std::cout);
-	TEST(log(x), 'x', 5, 6.1, std::cout);
-	
-	TEST((sin(cos(log((((((2 * (((1 + x) + 1) * 2)) * x) / x) ^ 2) ^ x))))), 
+	TEST(    x,  'x', 5,   1, std::cout);
+	TEST(    x,  'y', 5,   1, std::cout);
+	TEST(1 + x,  'x', 5,   1, std::cout);
+	TEST(x + 1,  'x', 5,   1, std::cout);
+	TEST(2 * x,  'x', 5,   2, std::cout);
+	TEST(x * 2,  'x', 5,   2, std::cout);
+	TEST(x * x,  'x', 5,  10, std::cout);
+	TEST(1 / x,  'x', 5,-0.04, std::cout);
+	TEST(x / 1,  'x', 5,   5, std::cout);
+	TEST(x / x,  'x', 5,   0, std::cout);
+	TEST(x ^ 2,  'x', 5,  10, std::cout);
+	TEST(2 ^ x,  'x', 5,   2, std::cout);
+	TEST(x ^ x,  'x', 5, 3154.49, std::cout);
+	TEST(sin(x), 'x', 5, -0.9589, std::cout);
+	TEST(cos(x), 'x', 5, 0.28366, std::cout);
+	TEST(log(x), 'x', 5, 1.60943, std::cout);
+
+	TEST((sin(cos(log((((((2 * (((1 + x) + 1) * 2)) * x) / x) ^ 2) ^ x))))),
 		'x', 5, 6.1, std::cout);
-	
+
 	// get the expression template tree
 	auto sample_tree = sin(cos(log((((((2 * (((1 + x) + 1) * 2)) * x) / x) ^ 2) ^ x))));
     std::cout << "expression tree: " << std::endl;
     const char *type_ptr = abi::__cxa_demangle(typeid(sample_tree).name(), 0, 0, NULL);
 	std::cout << type_ptr << std::endl;
-	
+
 	// fancy print:
 	auto print_expr = sin(2*x);
 	std::cout << "fancy print tree: " << print_expr.printValue('x') << std::endl;
